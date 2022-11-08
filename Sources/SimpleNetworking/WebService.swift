5ac0d2fb344e25460cfec352782f009d39cf844c
Nurlan Akylbekov <n.akylbekov@vlife.kz>
2022-11-08 22:36:20 +0600

    fix: network reachability

--- Sources/SimpleNetworking/WebService.swift
+++ Sources/SimpleNetworking/WebService.swift
@@ -15,13 +15,11 @@ public protocol WebService: AnyObject {
 
 public final class WebServiceImpl: WebService {
   
-  private let networkReachabilityManager = SCNetworkReachabilityFlags()
-  
   public init() {}
   
   public func requestToNetwork(endpoint: Endpoint, completion: @escaping (NetworkRequestResult) -> Void) {
     
-    guard networkReachabilityManager.contains(.reachable) else {
+    guard connectedToNetwork() else {
       completion(.noInternet)
       return
     }
@@ -50,5 +48,30 @@ public final class WebServiceImpl: WebService {
     return URLRequest(url: route.api.url,
                       cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
                       timeoutInterval: 10.0)
+  }
+  
+  func connectedToNetwork() -> Bool {
+    
+    var zeroAddress = sockaddr_in()
+    zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+    zeroAddress.sin_family = sa_family_t(AF_INET)
+    
+    guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+      $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+        SCNetworkReachabilityCreateWithAddress(nil, $0)
+      }
+    }) else {
+      return false
+    }
+    
+    var flags: SCNetworkReachabilityFlags = []
+    if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+      return false
+    }
+    
+    let isReachable = flags.contains(.reachable)
+    let needsConnection = flags.contains(.connectionRequired)
+    
+    return (isReachable && !needsConnection)
   }
 }
